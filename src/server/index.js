6c7e54814a1905871cee034d92ef1b44b08b9211
kimsh0117 <crea.id.1703@gmail.com>
2018-10-09 13:41:14 +0300

    modify server

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,39 @@
 const express = require("express");
-const router = require("./routes/routes");
-const bodyParser = require("body-parser");
 
+const bodyParser = require("body-parser");
+const mysql = require("mysql")
 const port = process.env.PORT || 3001;
 
 const app = express();
 
-app.use('/world', router);
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'world',
+  user: 'root',
+  password: 'ti4809py'
+});
+
+connection.connect(function(err){
+  if(err) {
+    console.error(err);
+    return;
+  } else {
+    console.log('connected')
+  }
+})
+
+app.get('/world/search', function(req, res) {
+  let country = (req.query.country);
+  let q = `SELECT Name, Region FROM country WHERE Name = '${country}'`
+  connection.query(q, function (err, results) {
+      if (err) throw err;
+      res.json(results); 
+  })
+
+});
 
 app.listen(port, ()=> {
   console.log("App listening on port" + port );
--- src/server/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const controller = require('../controllers');
-const router = express.Router();
-
-router.get('/search', controller.world.get);
-
-module.exports = router;
